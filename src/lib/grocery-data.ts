a4c5c9755e69e2445f6bde322a15cfe17dd4ab71
RAMPRADOP S 22CDR081 <rampradops.22csd@kongu.edu>
2025-10-03 23:44:10 +0000

    can you make it as huge dataset  and train to match and provide correct

--- src/lib/grocery-data.ts
+++ src/lib/grocery-data.ts
@@ -1,27 +1,63 @@
 
 export const groceryItems = new Set([
-    // English
+    // English Staples & Grains
     'rice', 'sugar', 'salt', 'dal', 'lentils', 'wheat', 'flour', 'oil',
     'ghee', 'milk', 'yogurt', 'curd', 'cheese', 'paneer', 'butter',
+    'tea', 'coffee', 'biscuit', 'bread', 'eggs', 'atta', 'maida', 'sooji', 'rava',
+    'poha', 'jaggery', 'vermicelli', 'pasta', 'noodles', 'oats', 'semolina',
+    'basmati', 'sona masuri', 'idli rice',
+
+    // English Vegetables
     'tomato', 'potato', 'onion', 'garlic', 'ginger', 'chilli', 'lemon',
     'carrot', 'cabbage', 'cauliflower', 'spinach', 'coriander', 'mint',
     'brinjal', 'eggplant', 'beans', 'peas', 'cucumber', 'capsicum',
-    'apple', 'banana', 'orange', 'mango', 'grapes', 'watermelon',
-    'turmeric', 'cumin', 'mustard', 'pepper', 'cardamom', 'cloves',
-    'tea', 'coffee', 'biscuit', 'bread', 'eggs', 'chicken', 'mutton',
-    'fish', 'soap', 'shampoo', 'toothpaste', 'detergent', 'water',
-    'soda', 'juice', 'chips', 'nuts', 'atta', 'maida', 'sooji', 'rava',
-    'poha', 'jaggery',
+    'beetroot', 'radish', 'ladyfinger', 'okra', 'pumpkin', 'bottle gourd',
+    'ridge gourd', 'snake gourd', 'drumstick', 'broccoli', 'mushroom',
+    'sweet potato', 'corn', 'lettuce', 'celery', 'bell pepper',
 
-    // Tamil (in Latin script for matching)
+    // English Fruits
+    'apple', 'banana', 'orange', 'mango', 'grapes', 'watermelon', 'pineapple',
+    'papaya', 'guava', 'pomegranate', 'strawberry', 'chickoo', 'sapota',
+    'muskmelon', 'coconut',
+
+    // English Spices & Masalas
+    'turmeric', 'cumin', 'mustard', 'pepper', 'cardamom', 'cloves', 'cinnamon',
+    'asafoetida', 'fenugreek', 'bay leaf', 'coriander powder', 'chilli powder',
+    'garam masala', 'sambar powder', 'rasam powder', 'fennel',
+
+    // English Meats
+    'chicken', 'mutton', 'fish', 'prawns', 'crab',
+
+    // English Household & Packaged Goods
+    'soap', 'shampoo', 'toothpaste', 'detergent', 'water', 'soda', 'juice',
+    'chips', 'nuts', 'cashews', 'almonds', 'raisins', 'dates', 'ketchup',
+    'jam', 'pickle', 'honey', 'vinegar',
+
+    // --- Tamil (in Latin script for matching) ---
+
+    // Tamil Staples & Grains
     'arisi', 'sarkarai', 'sakkarai', 'uppu', 'paruppu', 'gothumai', 'maavu', 'ennai',
-    'nei', 'paal', 'thayir', 'panneer', 'vennai',
+    'nei', 'paal', 'thayir', 'panneer', 'vennai', 'semiya', 'godhumai',
+    'vellam', 'karupatti',
+
+    // Tamil Vegetables
     'thakkali', 'urulaikilangu', 'vengayam', 'poondu', 'inji', 'milagai', 'elumichai',
     'karattu', 'muttakosu', 'kalipilavar', 'pasalai', 'kothamalli', 'pudhina',
     'kathirikai', 'keerai', 'avarakkai', 'pattani', 'vellarikkai', 'kodaimilagai',
+    'vendakkai', 'murungakkai', 'peerkangai', 'pudalangai', 'poosanikai',
+    'sakaravalli kizhangu', 'kalan',
+
+    // Tamil Fruits
     'appil', 'vazhaipazham', 'aranju', 'mampazham', 'thiratchai', 'tharpoosani',
+    'pappali', 'annasi', 'koyya', 'madhulai', 'thengai',
+
+    // Tamil Spices & Masalas
     'manjal', 'jeeragam', 'kadugu', 'milagu', 'elakkai', 'kirambu', 'lavangam',
-    'tea', 'thool', 'kaapi', 'biskat', 'roti', 'muttai', 'koli', 'kari',
-    'meen', 'soppu', 'shampu', 'palpod', 'salavai', 'thanni',
-    'vellam'
+    'pattai', 'perungayam', 'vendhayam', 'birinji ilai', 'sombu',
+
+    // Tamil Meats
+    'koli', 'kari', 'meen', 'eral', 'nandu',
+
+    // Tamil Household & Packaged Goods
+    'soppu', 'shampu', 'palpod', 'salavai', 'thanni'
 ]);
